3cc3553511aa441dfcd95a9afd33ddef18a0f3fc
Bodnar Florina-Alina <bodnarflorina55@gmail.com>
2023-12-17 17:54:26 +0200

    add-changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from html.parser import HTMLParser
 from urllib.parse import urljoin
-
+from collections import deque
 
 class Parser(HTMLParser):
     def __init__(self, url, tag, visited_links):
@@ -10,27 +10,33 @@ class Parser(HTMLParser):
         self.tag = tag
         self.links = []
         self.visited_links = visited_links
+        self.limit_links = 500
+        self.queue = deque([url])
         self.get_links()
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'a':
+        if tag == 'a' and len(self.visited_links) < self.limit_links:
             for attr, value in attrs:
                 if attr == 'href':
                     complete_url = urljoin(self.url, value)
                     if self.tag in complete_url and complete_url not in self.visited_links:
-                        print(complete_url)
                         self.visited_links.append(complete_url)
                         self.links.append(complete_url)
-                        parser = Parser(complete_url, self.tag, self.visited_links)
+                        self.queue.append(complete_url)
+
+                        if len(self.visited_links) >= self.limit_links:
+                            break
 
     def get_links(self):
-        try:
-            response = urlopen(self.url)
-            html = response.read()
-            # print(f"{html}")
-            self.feed(html.decode('utf-8'))
-        except Exception as e:
-            print(f"Error fetching HTML: {e}")
+        while self.queue and len(self.visited_links) < self.limit_links:
+            url = self.queue.popleft()
+            try:
+                response = urlopen(url)
+                html = response.read()
+                # print(f"{html}")
+                self.feed(html.decode('utf-8'))
+            except Exception as e:
+                print(f"Error fetching HTML for {url}: {e}")
 
     def print_links(self):
         print(f"Traseul parcurs de tool este:")
@@ -39,16 +45,22 @@ class Parser(HTMLParser):
 
 
 if __name__ == "__main__":
-    url = "https://docs.python.org/3/library/html.parser.html"
-    tag = "html.parser"
+    url = input("Introduceti link-ul paginii web: ")
+    while True:
+        try:
+            urlopen(url)
+            break
+        except Exception as e:
+            print(f"Error fetching HTML: {e}")
+            url = input("Introduceti un link valid: ")
+
+    tag = input("Introduceti tag-ul: ")
+
+    # url = "https://docs.python.org/3/library/html.parser.html"
+    # tag = "html.parser"
 
     visited_links = [url]
 
     parser = Parser(url, tag, visited_links)
 
-    # links = parser.links
-    # for link in links:
-    #     print(link)
-
     parser.print_links()
-
